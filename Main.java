6fee295164a1b0aeef4591b0d7dc189d64273c63
lindsay-kislingbury <79282205+lindsay-kislingbury@users.noreply.github.com>
2023-04-20 19:58:06 +0000

    Working version reads one file

--- Main.java
+++ Main.java
@@ -1,6 +1,80 @@
-class Main{
-  public static void main(String[] args){
-    Student testStudent = new Student();
-    System.out.println(testStudent.getGreeting());
+import java.io.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+class Main {
+  public static void main(String[] args) {
+    ArrayList<Student> students = new ArrayList<Student>(); 
+
+    //TODO: make this a function where user can enter file name
+    //and can be called multiple times
+    File file = new File("quizmidtermScores.txt");  
+    
+    try {
+      Scanner fileScnr = new Scanner(file);
+      
+      /* first line is the headers. 
+      *  read the line into an array of Strings, 
+      *  using \t (tab) whitespace as the delimiter
+      */
+      String[] headers = fileScnr.nextLine().split("\t");
+      
+      /* loop through the rest of the data. store in student objects
+      *  reads in one line at a time. splits the whole line into an array
+      */ 
+      String[] line;
+      Character type = ' ';
+      int score = 0;
+      String name = "";
+      while (fileScnr.hasNext()) { //keep looping until no more lines to read   
+        String lineJustRead = fileScnr.nextLine(); //store the line just read in
+        line = lineJustRead.split("\t"); //split the line into an array of Strings
+        
+        for(int i=0; i<headers.length; i++){ //for the # of columns(headers) i
+          if(headers[i].contains("Name")){   
+            name = line[i];
+          }
+          else if(headers[i].contains("quiz")){
+            type = 'Q';
+            score = Integer.parseInt(line[i]);
+          }
+          else if(headers[i].contains("midterm")){
+           type = 'M';
+           score = Integer.parseInt(line[i]);
+          }
+          else if(headers[i].contains("final")){
+            type = 'F';
+            score = Integer.parseInt(line[i]);
+          }
+          /* Check if the student already exits in the array
+          *  if they do, add score. Otherwise, create new Student
+          *  there are weird rules about arraylists. Can't add to it while
+          *  iterating through it - that's why this is weird. 
+          *  Better solution?
+          */
+          boolean exists = false;
+          if(!students.isEmpty()){
+            for(Student s : students){
+              if(s.equals(name)){
+                s.addScore(type, score);
+                exists = true;
+              }
+            }
+          }
+          if(!exists){
+            Student tempStudent = new Student(name);
+            tempStudent.addScore(type, score);
+            students.add(tempStudent);
+          }
+        }
+      }
+      fileScnr.close();
+    } catch (FileNotFoundException e) { //TODO: more exceptions? different kind?
+      e.printStackTrace();
+    }
+    
+    //Print Array of Students
+    for(Student s : students){ s.printInfo(); }
+
   }
 }
--- target/classes/Student.java
+++ target/classes/Student.java
@@ -1,7 +1,80 @@
+import java.util.ArrayList;
+
 public class Student {
-  private String greeting = "Hello World! ^.^";
-//
-  public String getGreeting(){
-    return greeting;
+  private String name;
+  private ArrayList<Integer> finalScores;
+  private ArrayList<Integer> midtermScores;
+  private ArrayList<Integer> quizScores;
+
+  // Default Constructor
+  public Student() {
+    this.name = "empty";
+    finalScores = new ArrayList<Integer>();
+    midtermScores = new ArrayList<Integer>();
+    quizScores = new ArrayList<Integer>();
+  }
+
+  // Non-Default Constructor
+  public Student(String name) {
+    this.name = name;
+    finalScores = new ArrayList<Integer>();
+    midtermScores = new ArrayList<Integer>();
+    quizScores = new ArrayList<Integer>();
+  }
+
+  /*
+   * TODO: possible methods
+   * score average?
+   * final grade?
+   */
+
+
+  public boolean equals(String name) {
+    if (this.name.equals(name))
+      return true;
+    else
+      return false;
+  }
+
+  // Add a score
+  public void addScore(Character type, int score) {
+    switch (type) {
+      case 'F':
+        finalScores.add(score);
+        break;
+      case 'M':
+        midtermScores.add(score);
+        break;
+      case 'Q':
+        quizScores.add(score);
+        break;
+    }
+  }
+
+  // Print
+  // TODO: this should be cleaner and more concise
+  public void printInfo() {
+    System.out.println(name + "'s Scores:");
+    if (!finalScores.isEmpty()) {
+      System.out.print("Final: ");
+      for (Integer finalScore : finalScores) {
+        System.out.print(finalScore + ", ");
+      }
+      System.out.println();
+    }
+    if (!midtermScores.isEmpty()) {
+      System.out.print("Midterm: ");
+      for (Integer midtermScore : midtermScores) {
+        System.out.print(midtermScore + ", ");
+      }
+      System.out.println();
+    }
+    if (!quizScores.isEmpty()) {
+      System.out.print("Quiz: ");
+      for (Integer quizScore : quizScores) {
+        System.out.print(quizScore + ", ");
+      }
+      System.out.println("\n");
+    }
   }
 }
